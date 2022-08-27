56557ffad46cdd6b2770e840126ad995c3ef8be5
juank27 <juankruiz27@gmail.com>
2022-08-27 11:14:33 -0500

    data obtenida, pais, region, lenguaje(tambien encriptado), tiempo de generar cada campo

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import requests
 from werkzeug.security import generate_password_hash, check_password_hash
 import json
 import pandas as pd
-import time
 
 def getData(url):
    r = requests.get(url)
@@ -10,28 +9,40 @@ def getData(url):
       return r.json()
    return None
 
+import time
 #generate json
-def generateJSON(data):
+def generateJSON(data, time=time):
    data_json = {}
    for i in data:
       try:
+         start = time.time()
          aux = {}
          aux['region'] = i['region']
-         aux['language'] = i['languages']
          name = i['name']['common']
+         encrypt= encryptSHA1(keys(i['languages']))
+         aux['language'] = encrypt
+         aux['time'] = ("{:.6f}".format(time.time() - start))
          data_json[name] = aux
+         #print(data_json)
       except:
          pass
-   print('Time: ', end - start)
    return data_json
+
+#keys
+def keys(language):
+   concat_lenguage = ''
+   for i in language:
+      value = language[i]
+      concat_lenguage += value + '-'
+   return concat_lenguage
 
 #execution time
 def time():
    pass
 
 #encrypt data whith sha1
-def encryptSHA1(country):
-   encrypt = generate_password_hash(country, 'sha1')
+def encryptSHA1(language):
+   encrypt = generate_password_hash(language, 'sha1')
    return encrypt
 
 #veryfy
@@ -62,7 +73,8 @@ def saveJSON(data):
 
 def main():
    data = getData('https://restcountries.com/v3.1/all')
-   generateJSON(data)
+   data_generate = json.dumps(generateJSON(data), indent=3)
+   saveJSON(data_generate)
 
 
 
@@ -82,13 +94,15 @@ response_json = json.loads(data)
 response_json = json.dumps(response_json, indent=3)
 print(response_json)
 '''
+# import time
 
-n = time.time()
-main()
-end = time.time()
-print('Time: ', end - n)
-# if __name__ == '__main__':
-#    n = time.time()
-#    main()
-#    end = time.time()
-#    print('Time: ', end - n)
+# start = time.time()
+
+
+
+# end = time.time()
+
+# print(format(end-start))
+
+if __name__ == '__main__':
+   main()
